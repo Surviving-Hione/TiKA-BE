7daed5539ed7da9e6902edd855fec862838f379b
xian0310567 <xian0310567@naver.com>
2022-04-15 23:04:27 +0900

    feat: 팀 가입 기능 추가

--- src/jointeam/jointeam.controller.ts
+++ src/jointeam/jointeam.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { JointeamService } from './jointeam.service';
 import { joinTeam as JoinTeamModel } from '@prisma/client';
@@ -15,11 +15,28 @@ export class JointeamController {
         return this.joinTeamService.getAll()
     }
 
-    
+    // 해당 ID로 가입된 팀 조회
     @Get(':id')
     async getUser(@Param('id') id: string): Promise<JoinTeamModel[]> {
         return this.joinTeamService.getTarget({
             userId: String(id)
+        })
+    }
+
+    @Post()
+    async addJoinTeam(
+        @Body() joinData: {
+            userId: string,
+            team_code: string
+        }
+    ): Promise<JoinTeamModel> {
+        return this.joinTeamService.joinTeam({
+            user: {
+                connect: { id: joinData.userId }
+            },
+            team: {
+                connect: { code: joinData.team_code }
+            }
         })
     }
 }
