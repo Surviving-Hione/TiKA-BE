5838cdb1c209b4406ed034d530cfd1d908b915be
xian0310567 <the_comma2537@naver.com>
2022-06-02 00:52:57 +0900

    style: Code Clean Up

--- src/jointeam/jointeam.service.ts
+++ src/jointeam/jointeam.service.ts
@@ -4,31 +4,30 @@ import { joinTeam, Prisma } from '@prisma/client';
 
 @Injectable()
 export class JointeamService {
-    constructor(
-        private readonly prismaService: PrismaService
-    ) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async getAll(): Promise<joinTeam[] | null> {
-        return this.prismaService.joinTeam.findMany(
-        )
-    }
+  async getAll(): Promise<joinTeam[] | null> {
+    return this.prismaService.joinTeam.findMany();
+  }
 
-    // 특정 조건을 타겟으로 하여 검색할때는 controller에서 직접 탐색
-    async getTarget(data: Prisma.joinTeamWhereInput): Promise<joinTeam[] | null> {
-        return this.prismaService.joinTeam.findMany({
-            where: data
-        })
-    }
+  // 특정 조건을 타겟으로 하여 검색할때는 controller에서 직접 탐색
+  async getTarget(data: Prisma.joinTeamWhereInput): Promise<joinTeam[] | null> {
+    return this.prismaService.joinTeam.findMany({
+      where: data,
+    });
+  }
 
-    async joinTeam(data: Prisma.joinTeamCreateInput): Promise<joinTeam | null> {
-        return this.prismaService.joinTeam.create({
-            data
-        })
-    }
+  async joinTeam(data: Prisma.joinTeamCreateInput): Promise<joinTeam | null> {
+    return this.prismaService.joinTeam.create({
+      data,
+    });
+  }
 
-    async deletejoin(data: Prisma.joinTeamWhereUniqueInput): Promise<joinTeam | null> {
-        return this.prismaService.joinTeam.delete({
-            where: data
-        })
-    }
+  async deletejoin(
+    data: Prisma.joinTeamWhereUniqueInput,
+  ): Promise<joinTeam | null> {
+    return this.prismaService.joinTeam.delete({
+      where: data,
+    });
+  }
 }
