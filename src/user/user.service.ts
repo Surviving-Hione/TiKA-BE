2a742e1b0e79ee5078263ef5e4f301476b29b4f8
xian0310567 <the_comma2537@naver.com>
2022-06-22 04:38:21 +0900

    refactor: user Service refactoring

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,40 +4,35 @@ import { user, Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-    constructor(
-        private readonly prismaService: PrismaService
-    ) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async getUser(userWhereUniqueInput: Prisma.userWhereUniqueInput): Promise<user | null> {
-        return this.prismaService.user.findUnique({ 
-            where: userWhereUniqueInput
-        });
-    }
+  async getUser(userWhereUniqueInput: Prisma.userWhereUniqueInput): Promise<user | null> {
+    return this.prismaService.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
 
-    async getAll(): Promise<user[] | null> {
-        return this.prismaService.user.findMany();
-    }
+  async getAll(): Promise<user[] | null> {
+    return this.prismaService.user.findMany();
+  }
 
-    async createUser(data: Prisma.userCreateInput): Promise<user | null> {
-        return this.prismaService.user.create({
-            data
-        });
-    }
+  async createUser(data: Prisma.userCreateInput): Promise<user | null> {
+    return this.prismaService.user.create({
+      data,
+    });
+  }
 
-    async updateUser(params: {
-        where: Prisma.userWhereUniqueInput;
-        data: Prisma.userUpdateInput;
-    }): Promise<user | null> {
-        const { where, data } = params;
-        return this.prismaService.user.update({
-            where,
-            data
-        })
-    }
+  async updateUser(params: { where: Prisma.userWhereUniqueInput; data: Prisma.userUpdateInput }): Promise<user | null> {
+    const { where, data } = params;
+    return this.prismaService.user.update({
+      where,
+      data,
+    });
+  }
 
-    async deleteUser(data: Prisma.userWhereUniqueInput): Promise<user | null> {
-        return this.prismaService.user.delete({
-            where: data
-        })
-    }
+  async deleteUser(data: Prisma.userWhereUniqueInput): Promise<user | null> {
+    return this.prismaService.user.delete({
+      where: data,
+    });
+  }
 }
